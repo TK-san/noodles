8ed3c33d05709a70839f73e856f24f65cb74578e
TK-san <toto.github0001@gmail.com>
2025-11-30 18:21:04 +0800

    Add Cloudflare Pages config

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/noodles/',
+  // Use /noodles/ for GitHub Pages, / for Cloudflare
+  base: process.env.CF_PAGES ? '/' : '/noodles/',
   server: {
     host: '0.0.0.0',
     port: 5173,
